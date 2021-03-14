36c913865292f148b7e406cda8f64b821fb82277
Hunachi <hanahanahunachi@gmail.com>
2021-03-14 22:37:30 +0900

    implement profile and so on..><!!

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,24 @@
 import kotlinx.browser.document
+import org.w3c.dom.HTMLButtonElement
+import org.w3c.dom.HTMLHeadingElement
+import org.w3c.dom.events.Event
 
 fun main() {
-    document.write("Hello, world!")
+    var flag = false
+    val button = document.getElementById("hunaButton") as HTMLButtonElement
+    val text = document.getElementById("hunaText") as HTMLHeadingElement
+    button.onClick{
+        flag = !flag
+        if (flag) {
+            button.textContent = "clicled"
+            text.textContent = "nyan"
+        } else {
+            button.textContent = "Button"
+            text.textContent = "wan"
+        }
+    }
+}
+
+fun HTMLButtonElement.onClick(event: (Event) -> Unit){
+    this.addEventListener("click", event)
 }
