8872126bf9c60ff9d3682965b8bfc959acf06101
Hunachi <hanahanahunachi@gmail.com>
2021-03-25 15:37:03 +0900

    add blog.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,9 @@
-import org.w3c.dom.HTMLButtonElement
+import org.w3c.dom.HTMLElement
 import org.w3c.dom.events.Event
 
 fun main() {
-    // TODO
 }
 
-fun HTMLButtonElement.onClick(event: (Event) -> Unit){
+fun HTMLElement.onClick(event: (Event) -> Unit) {
     this.addEventListener("click", event)
 }
---
+++ src/main/markdown/sechack_blog_content.md
@@ -0,0 +1,16 @@
+# SecHack365を修了しました．
+
+こんにちはふなちです！初ブログ！
+
+## ho
+
+### hogehoge
+
+
+```kotlin
+int hoge = 0
+```
+
+`h`
+
+#### hogehoge
